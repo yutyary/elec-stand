cef6970ae5247f0e6aed5982f046eda6d594e4f9
yutyary <yutyary@gmail.com>
2020-12-21 02:30:15 +0900

    基本ファイルの追加 サンプル画像の追加

---
+++ cap_test.py
@@ -0,0 +1,14 @@
+import cv2
+
+# VideoCapture オブジェクトを取得します
+capture = cv2.VideoCapture(0)
+
+ret, frame = capture.read()
+# resize the window
+windowsize = (800, 600)
+frame = cv2.resize(frame, windowsize)
+
+cv2.imwrite('test.tif', frame) 
+
+capture.release()
+cv2.destroyAllWindows()
